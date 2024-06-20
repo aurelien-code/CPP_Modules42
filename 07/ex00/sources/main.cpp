3d235370773ef1d299931afa47af29d76c61a8c5
aumarin <aumarin@student.42.fr>
2024-06-20 08:41:18 +0200

    put functions in hpp files such as required by the assignement

--- 07/ex00/sources/main.cpp
+++ 07/ex00/sources/main.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
 
-template <typename T>
-T min(T &a, T &b)
-{
-	return (a < b) ? a : b;
-}
-
-template <typename T>
-T max(T &a, T&b)
-{
-	return (a > b) ? a : b;
-}
-
-template <typename T>
-void swap(T &a, T &b)
-{
-	T tmp = a;
-	a = b;
-	b = tmp;
-}
+#include "functions.hpp"
 
 int main()
 {
--- 07/ex01/sources/main.cpp
+++ 07/ex01/sources/main.cpp
@@ -1,25 +1,5 @@
 #include <iostream>
-
-template <typename T, typename F>
-void iter(T *arr, size_t size, F function)
-{
-	for (size_t i = 0; i < size; i++)
-	{
-		function(arr[i]);
-	}
-	
-}
-
-template <typename T>
-void printval(T &value)
-{
-	std::cout << value << std::endl;
-}
-
-void increment(int &val)
-{
-	++val;
-}
+#include "iter.hpp"
 
 int main()
 {
